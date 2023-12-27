414af6c98c6eee0c1808483a621e9d82627c2f81
Takahiro HIMURA <taka@himura.jp>
2023-12-27 12:36:24 +0900

    .

--- attic/create_hnsw_index.cpp
+++ attic/create_hnsw_index.cpp
@@ -1,21 +1,25 @@
-#include <opencv2/core.hpp>
-#include <opencv2/core/ocl.hpp>
-#include <opencv2/imgcodecs.hpp>
-#include <opencv2/img_hash.hpp>
 #include <hnswlib/hnswlib.h>
 #include <iostream>
+#include <opencv2/core.hpp>
+#include <opencv2/core/ocl.hpp>
+#include <opencv2/img_hash.hpp>
+#include <opencv2/imgcodecs.hpp>
 
 int main(int argc, char **argv) {
   const int dim = 8;
   const int M = 16;
-  const int ef_construction = 200;  // Controls index search speed/build speed trade-off
+
+  // Controls index search speed/build speed trade-off
+  const int ef_construction = 200;
   const int max_elements = 100000;
   const std::string hnsw_path = "hnsw.bin";
 
   cv::ocl::setUseOpenCL(false);
 
   hnswlib::L2Space space(dim);
-  std::unique_ptr<hnswlib::HierarchicalNSW<float>> alg_hnsw(new hnswlib::HierarchicalNSW<float>(&space, max_elements, M, ef_construction));
+  std::unique_ptr<hnswlib::HierarchicalNSW<float>> alg_hnsw(
+      new hnswlib::HierarchicalNSW<float>(&space, max_elements, M,
+                                          ef_construction));
 
   cv::Ptr<cv::img_hash::PHash> phash = cv::img_hash::PHash::create();
   auto convf = [](unsigned char a) { return static_cast<float>(a); };
--- attic/search_hnsw.cpp
+++ attic/search_hnsw.cpp
@@ -1,13 +1,14 @@
-#include <opencv2/core.hpp>
-#include <opencv2/core/ocl.hpp>
-#include <opencv2/imgcodecs.hpp>
-#include <opencv2/img_hash.hpp>
 #include <hnswlib/hnswlib.h>
 #include <iostream>
+#include <opencv2/core.hpp>
+#include <opencv2/core/ocl.hpp>
+#include <opencv2/img_hash.hpp>
+#include <opencv2/imgcodecs.hpp>
 
 int main(int argc, char **argv) {
   if (argc != 2) {
-    std::cerr << "must input the path of image: search_hnsw lena.jpg" << std::endl;
+    std::cerr << "must input the path of image: search_hnsw lena.jpg"
+              << std::endl;
     return -1;
   }
 
@@ -19,7 +20,8 @@ int main(int argc, char **argv) {
   cv::ocl::setUseOpenCL(false);
 
   hnswlib::L2Space space(dim);
-  std::unique_ptr<hnswlib::HierarchicalNSW<float>> alg_hnsw(new hnswlib::HierarchicalNSW<float>(&space, hnsw_path));
+  std::unique_ptr<hnswlib::HierarchicalNSW<float>> alg_hnsw(
+      new hnswlib::HierarchicalNSW<float>(&space, hnsw_path));
 
   cv::Ptr<cv::img_hash::PHash> phash = cv::img_hash::PHash::create();
   auto convf = [](unsigned char a) { return static_cast<float>(a); };
@@ -33,11 +35,13 @@ int main(int argc, char **argv) {
   std::vector<float> buf(dim);
   std::transform(hash.datastart, hash.dataend, buf.data(), convf);
 
-  std::priority_queue<std::pair<float, hnswlib::labeltype>> result = alg_hnsw->searchKnn(buf.data(), numNeighbors);
+  std::priority_queue<std::pair<float, hnswlib::labeltype>> result =
+      alg_hnsw->searchKnn(buf.data(), numNeighbors);
 
   while (!result.empty()) {
     auto label = result.top();
-    std::cout << "label: " << label.second << ", distance: " << label.first << std::endl;
+    std::cout << "label: " << label.second << ", distance: " << label.first
+              << std::endl;
     result.pop();
   }
 }
